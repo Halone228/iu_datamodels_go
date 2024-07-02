e35098db1dfbdedd6738e028f2a2ba36bd08098c
Halone AKA Kirill <haloneprog@gmail.com>
2024-07-02 16:19:36 +0300

    [-] Renaming routes

--- routes.go
+++ routes.go
@@ -1,8 +1,8 @@
 package iu_datamodels_go
 
 const (
-	MINERAL = "/mineral"
-	TAG     = "/tag"
-	SOURCE  = "/source"
-	VEIN    = "/vein"
+	MineralRoute = "/mineral"
+	TagRoute     = "/tag"
+	SourceRoute  = "/source"
+	VeinRoute    = "/vein"
 )
